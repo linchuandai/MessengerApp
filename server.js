b3674d3b96c10618a041c781b5ac02b0d00c10b1
Leon Dai <linchuandai@gmail.com>
2018-02-03 21:44:41 -0500

    database and other stuff

--- server.js
+++ server.js
@@ -3,28 +3,56 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
-]
+mongoose.Promise = Promise
+
+var dbUrl = 'mongodb://user:password@ds125058.mlab.com:25058/message-app'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
-app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
+app.post('/messages', async (req, res) => {
+    var message = new Message(req.body)
+
+    var savedMessage = await message.save()
+
+    console.log('saved')
+    var censored = await Message.findOne({message: 'badword'})
+
+    if(censored)
+        await Message.remove({_id: censored.id})
+    else
+        io.emit('message', req.body)
+
     res.sendStatus(200)
+
+//    .catch((err) => {
+//        res.sendStatus(500)
+//        return conosle.error(err)
+//    })
+
 })
+
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
